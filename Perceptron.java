98105de3ec6c2b97d5229b975fdb4d379e654f90
yuuto-0226 <69916482+yuuto-0226@users.noreply.github.com>
2020-09-07 23:07:14 +0800

    Add files via upload

--- Perceptron.java
+++ Perceptron.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Perceptron{
     public static void main(final String[] args) {
-        final Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         final train tr = new train(0.02); // 生成物件
         tr.fit(0,200);    //訓練
         //完成
