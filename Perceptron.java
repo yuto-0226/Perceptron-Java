b42b5e3d1359e785c087d0063db2db13a849dbf3
yuuto-0226 <69916482+yuuto-0226@users.noreply.github.com>
2020-09-11 01:07:31 +0800

    v1.0
    第一次的成功qwq

--- Perceptron.java
+++ Perceptron.java
@@ -2,17 +2,23 @@ import java.util.*;
 public class Perceptron{
     public static void main(final String[] args) {
         Scanner sc=new Scanner(System.in);
-        final train tr = new train(0.02); // 生成物件
+        final train tr = new train(1.45); // 生成物件
         tr.fit(0,200);    //訓練
         //完成
         int x;
         double net;
         while(sc.hasNext()){
-            System.out.print("[Input]");
-            x=sc.nextInt();
-            net=tr.Sigma(x);
-            System.out.println();
-            System.out.println("[Output]"+actFunction.Sigmoid(net));
+            try {
+                x=sc.nextInt();
+                net=tr.Sigma(x);
+                System.out.println();
+                System.out.println(" >> "+actFunction.Sigmoid(net));
+            
+            } catch (InputMismatchException e) {
+                System.out.println("System Error >> "+e);
+                x=0;
+                break;
+            }
         }
     }
 }
--- train.java
+++ train.java
@@ -5,44 +5,62 @@ public class train {
     private double w;    // 權重
     private double net;    // x加總
     private double t;    // 預估值
-    private int count;    // 預估值
+    private int count;    // 更新次數
 
     public train(final double Eta) {
         this.eta = Eta;
-        this.e = 0;
+        this.e = 15650;
         this.w = 0;
         this.net = 0;
         this.t = 0;
         this.count = 0;
     }
 
+    public float per(int w, double[] x) {
+
+        return (1 - (w * 1.00f / x.length));
+    }
+
     public void fit(double max, double min) {
          Data d = new Data();
-        double y;
-        double x;
+        double[] y=new double[52005];
+        double[] x=new double[52005];
         count = 1;
-        while (count<=1000) {
-            x = d.data(max, min);
-            t = (x > 100) ? 1 : 0; // 設定預計值
-            net = Sigma(x);
-            y = actFunction.Sigmoid(net);
-/*            if ((eta * (t - y) * x) == 0) {
-*               System.out.println("--Finished--\n");
-*                break;
-*            }
-*/
+        int wr=0;    //錯誤次數
+        int i=0;    //陣列資料
+        float per=0;
+        while (count!=52005) {
+            x[i] = d.data(max, min);
+            t = (x[i] > 100) ? 1 : 0; // 設定預計值
+            net = Sigma(x[i]);
+            y[i] = actFunction.Sigmoid(net);
+
             // 更新
-            w += eta * (t - y) * x;
-            e += eta * (t - y) * 1;
+            w += eta * (t - y[i]) * x[i];
+            e += eta * (t - y[i]) * 1;
+
+            if(t-y[i]!=0){
+                wr+=1;
+            }
+            per=per(wr, x);
             //印出
-            System.out.println("[" + count + "]input=" + x + " ,w=" + w + " ,e=" + e + " ,Bias=" + (eta * (t - y) * x));
+            System.out.println("[" + count + "]Input="+x[i] +" ,Output="+ y[i] +" ,w="+w+" ,e="+e+" ,Bias="+(eta*(t-y[i])*x[i])+" ,erro="+wr+" >> "+per);
+
+
             count += 1;
+            i+=1;
+
+//            if (per>=0.9999997) {
+//                System.out.println("--Finished--\n");
+//                 break;
+//             }
         }
+        System.out.println("\n");
 
     }
     //加總
     public double Sigma(final double x) {
-        net=(x * w + e);
+        net=(x * w - e);
         return net;
     }
     
